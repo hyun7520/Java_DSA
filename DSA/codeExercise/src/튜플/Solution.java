6b9a7d50fe53728de22d32ec55197fc5da79ae86
hyun7520 <sanghyun123452@gmail.com>
2023-11-09 20:32:16 +0900

    upload algorithm study
    problems from programmers

--- DSA/codeExercise/src/튜플/Solution.java
+++ DSA/codeExercise/src/튜플/Solution.java
@@ -1,19 +1,38 @@
 package 튜플;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.List;
 
 public class Solution {
     public int[] solution(String s) {
-        int[] answer = {};
 
-        String re = s.replaceAll("[\\{,\\}]", " ");
 
-//        System.out.println(re);
+        List<Integer> AL = new ArrayList<>();
 
-        String[] newS = s.split(" ");
+        String[] re = s.substring(2, s.length() - 2).replaceAll("[}{]", " ").split(" , ");
 
-        Arrays.sort(newS, (o1, o2) -> Integer.compare(o1.length(), o2.length()));
+        Arrays.sort(re, (o1, o2) -> Integer.compare(o1.length(), o2.length()));
+
+        String[][] newS = new String[re.length][];
+
+        for(int i = 0; i < re.length; i++) {
+            newS[i] = re[i].split(",");
+        }
+
+        for(String[] arr: newS) {
+            for(String temp : arr) {
+                if(!AL.contains(Integer.parseInt(temp))) {
+                    AL.add(Integer.parseInt(temp));
+                }
+            }
+        }
+
+        int[] answer = new int[AL.size()];
+
+        for(int i = 0; i < AL.size(); i++) {
+            answer[i] = AL.get(i);
+        }
 
         return answer;
     }
@@ -22,6 +41,6 @@ public class Solution {
 class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        sol.solution("{{2},{2,1},{2,1,3},{2,1,3,4}}");
+        sol.solution("{{1,2,3},{2,1},{1,2,4,3},{2}}");
     }
 }
